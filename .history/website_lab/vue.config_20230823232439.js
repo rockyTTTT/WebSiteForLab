36ef997207f5b6acd056bea046ddfc253b943e5f
rockyTTTT <107424045+rockyTTTT@users.noreply.github.com>
2023-08-24 00:14:47 +0800

    初始化
    初始化

---
+++ .history/website_lab/vue.config_20230823232439.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require('@vue/cli-service')
+const { config } = require('vue/types/umd')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  devServer:{
+    port: 7070
+  },
+  chainWebpack: config =>{
+    config.plugin('html')
+    .tap(args => {
+      args[0].title = "Zhanglab";
+      return args;
+    })
+  }
+})
