87050dede13716d6bc29f39b65981859e3b6b1c8
rockyTTTT <107424045+rockyTTTT@users.noreply.github.com>
2023-09-09 21:54:48 +0800

    修改

--- web-zhang/.history/src/util/rem_20230909215345.js
+++ web-zhang/.history/src/util/rem_20230909215345.js
@@ -1,8 +1,8 @@
 const baseSize = 151
 
 function setRem() {
-  console.log(document.documentElement.clientWidth);
-  const scale = document.documentElement.clientWidth / 1511;
+    console.log(document.documentElement.clientWidth);
+  const scale = document.documentElement.clientWidth / 1100;
   document.documentElement.style.fontSize = (baseSize * scale) + 'px';
 
 }
--- web-zhang/.history/src/util/rem_20230909215351.js
+++ web-zhang/.history/src/util/rem_20230909215351.js
@@ -1,7 +1,7 @@
 const baseSize = 151
 
 function setRem() {
-  console.log(document.documentElement.clientWidth);
+    console.log(document.documentElement.clientWidth);
   const scale = document.documentElement.clientWidth / 1511;
   document.documentElement.style.fontSize = (baseSize * scale) + 'px';
 
